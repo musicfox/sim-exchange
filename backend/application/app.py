679a37e992fbb938f56a6732ca0a8fe711b13417
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-05-08 12:52:55 -0500

    :construction: barebones application structure passes initial instantiation test

--- backend/application/app.py
+++ backend/application/app.py
@@ -24,8 +24,8 @@ from flask_login import (
     UserMixin,
 )
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import User
-from dbconfig import DATABASE_URI
+#from models import User
+#from dbconfig import DATABASE_URI
 
 # login/logout
 
@@ -35,74 +35,20 @@ login_manager.session_protection = "strong"
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    #app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
     # generate random session key
     app.secret_key = os.urandom(16)
 
-    from models import db
+    #from models import db
 
-    db.init_app(app)
-    login_manager.init_app(app)
+    #db.init_app(app)
+    #login_manager.init_app(app)
     return app
 
 
 app = create_app()
-# define unauthorized access
-@login_manager.unauthorized_handler
-def unauthorized():
-    """
-    View for unauthorized access.
-    """
-    return redirect(url_for("login"))
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
-# @app.route('/investors/login', methods=['GET', 'POST'])
-# def login():
-#    """
-#    The investor login page.
-#
-#    Investor users are given an email and password.
-#    """
-#
-#    form = InvestorsLoginForm(request.form)
-#    if request.method == 'GET':
-#        return render_template("investors-login.html", form=form) # no form available
-#
-#
-#    if request.method == 'POST' and form.validate():
-#        email = request.form['email']
-#        user = User.query.filter_by(email=email).first()
-#        psswd = request.form['password']
-#
-#        if user:
-#            if user.verify_password(psswd):
-#                login_user(user)
-#                session['email'] = email
-#                return redirect('/investors')
-#            return render_template("investors-login.html", form = form)
-#        return render_template("investors-login.html", form = form)
-#
-#    return render_template("investors-login.html", form = form)
-#
-# @app.route('/investors/logout')
-# @login_required
-# def logout():
-#    # remove the email from the session if extant
-#    session.pop('email', None)
-#    logout_user()
-#    return redirect(url_for('landing_page'))
-#
-# @app.route('/investors')
-# @fresh_login_required
-# def investors():
-#    return render_template("investors.html")
--- backend/application/conftest.py
+++ backend/application/conftest.py
@@ -1,3 +1,48 @@
+"""
+# `conftest.py`
+
+Pytest configuration file including test fixtures and other setup items.
+
+## Usage
+```python
+python -m pytest -v -d --xt popen//python=python3 # using popen
+# boring
+python -m pytest -v
+# also boring
+pytest -v
+```
+"""
+import pytest 
 
 
+pytest.fixture(scope='module')
+def contributors():
+    # TODO Scrape README.md for names
+    return [
+        '@sssyzz',
+        '@RKoulikova', 
+        '@yzhao0429',
+        '@sebastientradair',
+        '@philipmuh',
+        '@Zhrong25', 
+    ]
 
+pytest.fixture(scope='module')
+def investor_user(): # TODO Implement investor_user
+    pass # return a simulated investor user
+
+pytest.fixture(scope='module')
+def artist_user(): # TODO Implement artist_user
+    pass # return a simulated artist user
+
+pytest.fixture(scope='module')
+def investorUsers(): # TODO Implement investorUsers
+    pass #yield an iterable of generated investor users
+
+pytest.fixture(scope='module')
+def assetUsers(): # TODO IMplement assetUsers
+    pass #yield an iterable of generated asset users
+
+pytest.fixture(scope='module')
+def arrivalPath(): # TODO Implement arrivalPath
+    pass # generate an arrival path based on the wikipedia data of actors
